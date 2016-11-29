0efc92439298e44c198a2dbaca3346649aa384e9
nalrayes <na1487@nyu.edu>
2016-11-28 19:08:40 -0500

    documentation added for test

--- test.py
+++ test.py
@@ -4,7 +4,13 @@ import SpecialCharacters
 import pdb
 from collections import deque
 
+
 def get_pronunciation(sentence):
+    """
+    need espeak installed for this to work, this process returns the 
+    pronounciaiton as espeak would return it by running espeak as a
+    subprocess.
+    """
     output = subprocess.check_output(['espeak', '-x', '-q', sentence])
     return output
 
@@ -14,24 +20,37 @@ pronunciation = get_pronunciation(query)
 list_proc = pronunciation.split()
 
 for term in list_proc:
+    # for each term provided
     print(term)
     term = str(term)
+    # we dont want the right hanging aposrophe that espeak returns
     term = term.rstrip("\"")
+    # or the left b", all we need is the pronounciation
     term = term.lstrip("b\"")  
     syl_count = 0
+    # we contruct the rhyme family by figuing out the current syllable,
+    # the last syllable in the word is the rhyme family
     rhyme_fam = ''
+    # we need the previous character to realize if we have reached a new
+    # syllable or not.
     prev_c = ''
     for c in term:
+        # for each character in the term
         c = str(c)
+        # if the character is not a vowel, we should include it in the rhyme family
         if c not in SpecialCharacters.vowels:
             rhyme_fam += c
             prev_c = c
+        # if the character is a vowel, we run into two possible situations
         else:
+            # if we start a new word or the previous character is not a vowel,
+            # we have started a new syllable and its time to reset
             if prev_c  == '' or prev_c not in SpecialCharacters.vowels:
                 syl_count += 1
                 rhyme_fam = c
                 prev_c = c
-            else:
+            # otherwise the character is a part of the current syllable 
+            else: 
                 rhyme_fam += c
                 prev_c = c
 
