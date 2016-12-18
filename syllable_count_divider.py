a2d573e04f9985eab8da3b9747ce0626a8aeab37
nalrayes <na1487@nyu.edu>
2016-12-18 10:59:37 -0500

    counts rhymes and divides into rhyme --> POS tag --> syllable count --> word

--- syllable_count_divider.py
+++ syllable_count_divider.py
@@ -8,10 +8,11 @@ import pprint
 import CMUTweetTagger as CMU
 import re
 import sys
+import string
 from collections import defaultdict
 from collections import Counter
 
-def count(sentances):
+def syl_to_word(sentances):
 	result = defaultdict(Counter)
 	for sentance in sentances:
 		info = test.get_words_info(sentance)
@@ -20,8 +21,24 @@ def count(sentances):
 			result[i[0]][i[3]] += 1
 	return result
 
+def rhyme_to_POS(sentances):
+	result = defaultdict(lambda : defaultdict(defaultdict))
+	tagged_tweets = CMU.runtagger_parse(sentances)
+	for tweet in tagged_tweets:
+		for word in tweet:
+			cur_word = word[0]
+			tag = word[1]
+			info = test.get_words_info(cur_word)[0]
+			rhyme = info[1]
+			syl_count = info[0]
+			result[rhyme][tag][syl_count] = cur_word
+	return result
+
+
+
 with open(sys.argv[1]) as f:
 	lines = f.readlines()
-res = count(lines)
 pp = pprint.PrettyPrinter(indent=4)
+res = rhyme_to_POS(lines)
 pp.pprint(res)
+
