048283993e44f610c79a0c18c013de4fc4d2fbe5
nalrayes <na1487@nyu.edu>
2016-12-19 04:10:36 -0500

    rhyming! it rhymes!

--- Process.py
+++ Process.py
@@ -6,7 +6,7 @@ from nltk.tokenize import sent_tokenize
 from numpy.random import choice
 import string
 import re
-
+from syllable_count_divider import rhyme_to_POS
 #takes in a list of sets. Each set contains a list of the words, their tags, and the probabilities
 def get_tag_frequencies(tagged_words):
 	tag_table = {}
@@ -102,7 +102,7 @@ def create_rules(tags):
 	return rules
 
 
-def generate(rules,tag_table, pos_syls):
+def generate_firsttwo(tag_table, pos_syls):
 	tag = "Start"
 	sentence = ""
 	syllable_count = 0
@@ -112,9 +112,12 @@ def generate(rules,tag_table, pos_syls):
 	rhyme_1 = ''
 	rhyme_2 = 	''
 	while tag != "end":
+		cur_syl_count = syllable_count
 		next_tag = choice(list(rules[tag].keys()),1,list(rules[tag].values()))[0]
 		if next_tag == "end":
-			break
+			next_tag = "Start"
+			continue
+		words_info = test.get_words_info(sentence)
 		if (syllable_count >= 10 and end):
 			rhyme_2 = words_info[len(words_info) - 1][1]
 			break
@@ -124,6 +127,7 @@ def generate(rules,tag_table, pos_syls):
 			syllable_count = 0
 			end = True
 			continue
+
 		#generate word from tag
 		table_by_tag = [k for k in pos_syls[next_tag].keys() if k <= (10 - syllable_count)]
 		tries_with_prev_tag = 0
@@ -138,14 +142,16 @@ def generate(rules,tag_table, pos_syls):
 
 		num_syls = choice(table_by_tag, 1)[0]
 		word = choice(pos_syls[next_tag][num_syls], 1)[0]
+		cur_syl_count += test.get_words_info(word)[0][0]
+
 		if not re.search("[aeiou]", str(word)) :
 			continue
 		table = str.maketrans({key: None for key in string.punctuation})
 		word = str(word).translate(table)  
 		if not word:
 			continue
-		##check syllable count
-		words_info = test.get_words_info(sentence + " " + word)
+		# ##check syllable count
+		# words_info = test.get_words_info(sentence + " " + word)
 
 		tries_with_syl_count = 0
 		possible_tries_syl = len(pos_syls[next_tag][num_syls])
@@ -179,6 +185,107 @@ def generate(rules,tag_table, pos_syls):
 		tag = next_tag
 	return (sentence, rhyme_1, rhyme_2)
 
+def generate_lasttwo(tag_table, pos_syls, rhyme_pos_syls, rhyme1, rhyme2):
+	tag = "Start"
+	sentence = ""
+	syllable_count = 0
+	deadlist_tag = []
+	deadlist_syl = []
+	deadlist_rhyming = []
+	end = False
+	cur_rhyme = rhyme1
+	pos_syls_backup = pos_syls
+	while tag != "end":
+		cur_syl_count = syllable_count
+		next_tag = choice(list(rules[tag].keys()),1,list(rules[tag].values()))[0]
+		if next_tag == "end":
+			next_tag = "Start"
+			continue
+		if (syllable_count >= 10 and end):
+			# rhyme_2 = words_info[len(words_info) - 1][1]
+			break
+		if (syllable_count == 10):
+			cur_rhyme = rhyme2
+			sentence += "\n"
+			syllable_count = 0
+			end = True
+			continue
+
+		#generate word from tag
+		table_by_tag = [k for k in pos_syls[next_tag].keys() if k <= (10 - syllable_count)]
+		rhyme_POS = rhyme_pos_syls[cur_rhyme]
+		# newly added
+		table_by_tag_rhyme = [k for k in rhyme_POS[next_tag].keys() if k == (10 - syllable_count)]
+		if table_by_tag_rhyme:
+			table_by_tag = table_by_tag_rhyme
+			pos_syls = rhyme_POS
+		tries_with_prev_tag = 0
+		possible_tries_prev_tag = len(pos_syls)
+		if not table_by_tag:
+			deadlist_tag.append(next_tag)
+			tries_with_prev_tag += 1
+			if (tries_with_prev_tag > possible_tries_prev_tag):
+				raise Exception("The corpus provided is not big enough")
+			continue
+
+
+		num_syls = choice(table_by_tag, 1)[0]
+		word = choice(pos_syls[next_tag][num_syls], 1)[0]
+		cur_word_info = test.get_words_info(word)
+		cur_syl_count += cur_word_info[0][0]
+		if (cur_syl_count == 10):
+			if cur_word_info[0][1] != cur_rhyme:
+				try:
+					word = choice(rhyme_pos_syls[cur][next_tag][num_syls])[0]
+				except:
+					deadlist_rhyming.append(next_tag)
+					next_tag = tag
+					cur_syl_count -= cur_word_info[0][0]
+					continue
+
+		if not re.search("[aeiou]", str(word)) :
+			continue
+		table = str.maketrans({key: None for key in string.punctuation})
+		word = str(word).translate(table)  
+		if not word:
+			continue 
+		# ##check syllable count
+		# words_info = test.get_words_info(sentence + " " + word)
+
+		tries_with_syl_count = 0
+		possible_tries_syl = len(pos_syls[next_tag][num_syls])
+
+		tries_with_tag = 0
+		possible_tries_tag = len(pos_syls[next_tag])
+
+		if (tries_with_syl_count > possible_tries_syl):
+			deadlist_syl.append(num_syls)
+			table_by_tag = [k for k in pos_syls[next_tag].keys() if k < (10 - syllable_count) and k not in deadlist_syl]
+			if table_by_tag:	
+				num_syls = choice(table_by_tag, 1)[0]
+				possible_tries_syl = len(pos_syls[next_tag][num_syls])
+				continue
+			tries_with_tag += 1
+			tries_with_syl_count = 0
+			if (tries_with_tag > possible_tries_tag):
+				next_tag = choice(list(rules[tag].keys()),1,list(rules[tag].values()))[0]
+				tries_with_tag = 0
+				tries_with_syl_count = 0
+				possible_tries_tag = len(pos_syls[next_tag])
+				tries_with_prev_tag += 1
+				if (tries_with_prev_tag > possible_tries_prev_tag):
+					raise Exception("The corpus provided is not big enough") 
+
+		word_info = test.get_words_info(word)
+		syllable_count += word_info[0][0]
+		sentence += word + " "
+		deadlist_syl = []
+		deadlist_tag = []
+		deadlist_rhyming = []
+		tag = next_tag
+		pos_syls = pos_syls_backup
+	return sentence
+
 
 def check_seq_word(word_info, prev_stress_OG):
 	syllables = word_info[2]
@@ -209,7 +316,17 @@ x = CMU.runtagger_parse(sent_tokenize(file.read())) ##we should strip the senten
 rules = create_rules(x)
 tag_table = get_tag_frequencies(x)
 syl_rules = get_pos_syllables(x)
-print(generate(rules,tag_table, syl_rules))
+rhyme_pos_table = rhyme_to_POS(x)
+result1 = generate_firsttwo(rules, syl_rules)
+r1 = result1[1]
+r2 = result1[2]
+firsttwo = result1[0]
+result2 = generate_lasttwo(rules, syl_rules, rhyme_pos_table, r1, r2)
+lasttwo = result2
+print(firsttwo)
+print(lasttwo)
+# pdb.set_trace()
+print()
 # for line in file:
 # 	sent.append(CMU.runtagger_parse(line))
 # 	count += 1
